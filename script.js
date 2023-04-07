96955559d01b8ebeaa73342f4add2b17277323b3
NMGVox <nijeemgreene@gmail.com>
2023-04-07 15:47:19 -0400

    Add logic for adding alternating marks to board

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var gameBoard = (function () {
+    let spaces = [];
     let moves = 0;
     
     function _makeBoard(){
@@ -8,29 +9,89 @@ var gameBoard = (function () {
         for(let i = 0; i < 9; i++){
             let newWrap = document.createElement('div');
             let newSpace = document.createElement('img');
+            
             newWrap.className = ('wrapper');
+            newSpace.className = ('space');
+
             newWrap.appendChild(newSpace);
             newWrap.setAttribute('index', i);
             board.appendChild(newWrap);
+            spaces.push('');
         }
         document.querySelector('body').appendChild(board);
         document.getElementById('start').style.display = 'none';
+        console.log(spaces)
     };
 
-    
-
+    function _updateSpaces(i, m) {
+        spaces[i] = m;
+    }
     return {
         initializeGame: function () {
             _makeBoard();
+        },
+        update: function (indx, mark) {
+            _updateSpaces(indx, mark);
+            console.log(spaces);
+        },
+        spaceAvailable: function(i) {
+            return spaces[i];
         }
     };
 })();
 
+var controller = (function () {
+    let players = [];
+    let activePlayer;
+
+    function _makePlayers() {
+        players.push(playerFactory("human", 'X'));
+        players.push(playerFactory("human", 'O'));
+        activePlayer = players[0];
+    }
+
+    function _addMark(e) {
+        if(gameBoard.spaceAvailable(e.target.getAttribute('index')) !== ''){
+            return;
+        }
+
+        if (activePlayer.mark === 'O'){
+            e.target.firstChild.src = './images/circle.svg';
+        }else {
+            e.target.firstChild.src = './images/cross.svg';
+        }
+        e.target.firstChild.style.display = 'block';
+
+        gameBoard.update(e.target.getAttribute('index'), activePlayer.mark);
+        if(activePlayer === players[0]){
+            activePlayer = players[1]
+        } else{
+            activePlayer = players[0];
+        }
+    }
+
+    return {
+        addMark: function (e) {
+            _addMark(e);
+        },
+        makePlayers: function() {
+            _makePlayers();
+        }
+    }
+})();
+
+function playerFactory(ptype, mark) {
+    return {ptype, mark};
+}
+
 
 document.querySelector('body').addEventListener('pointerdown', (event)=>{
     if(event.target.classList.contains('wrapper')) {
-        console.log("Sorta works.");
+        controller.addMark(event);
     }
 })
 
-document.querySelector("#start").addEventListener('pointerdown', gameBoard.initializeGame);
+document.querySelector("#start").addEventListener('pointerdown', (event) =>{
+    controller.makePlayers();
+    gameBoard.initializeGame();
+});
